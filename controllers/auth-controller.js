a4b0e77ffd8fb785dbc27a012edb839dc3577d06
Vira <35749970+Vestanra@users.noreply.github.com>
2023-12-13 19:22:51 +0200

    hw5

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,19 +4,25 @@ import { ctrlWrapper } from "../decorators/index.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotevn from "dotenv";
+import path from "path";
+import Jimp from "jimp";
+import gravatar from "gravatar";
 
 dotevn.config();
 
 const { SECRET_KEY } = process.env;
+const avatarsPath = path.resolve("public", "avatars");
 
 const register = async (req, res) => {
     const { email, password } = req.body;
+    const avatarURL = gravatar.url(email);
+
     const result = await User.findOne({ email });
     if (result) {
         throw HttpError(409, "Email in use");
     }
     const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({...req.body, password: hashPassword});
+    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
     res.status(201).json({
         "user": {
             "email": newUser.email,
@@ -27,7 +33,7 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
     const { email, password } = req.body;
-    console.log(SECRET_KEY)
+    console.log(res)
 
     const user = await User.findOne({ email });
     if (!user) {
@@ -70,11 +76,30 @@ const logout = async (req, res) => {
 
 const updateSubscription = async (req, res) => {
     const { _id, email } = req.user;
+    console.log(req.body)
     const userUpdate = await User.findByIdAndUpdate(_id, req.body)
     res.json({
         email,
         subscription: userUpdate.subscription,
     })
+};
+
+const updateAvatar = async (req, res) => {
+    const { _id } = req.user;
+    if (!req.file) {
+        throw HttpError(400, "missing fields avatars")
+    }
+    const { path: oldPath, originalname } = req.file;
+    const fileName = `${_id}_${originalname}`
+    const newPath = path.join(avatarsPath, fileName);
+
+    const image = await Jimp.read(oldPath);
+    image.cover(250, 250);
+    await image.writeAsync(newPath);
+
+    const avatarURL = path.join("avatars", fileName);
+    await User.findByIdAndUpdate(_id, avatarURL);
+    res.json({avatarURL})
 };
 
 export default {
@@ -83,4 +108,5 @@ export default {
     getCurrent: ctrlWrapper(getCurrent),
     logout: ctrlWrapper(logout),
     updateSubscription: ctrlWrapper(updateSubscription),
+    updateAvatar: ctrlWrapper(updateAvatar),
 }
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,3 +2,4 @@ export { default as isEmptyBody } from "./isEmptyBody.js";
 export { default as isValidId } from "./isValidId.js";
 export { default as isEmptyFavoriteBody } from "./isEmptyFavoriteBody.js";
 export { default as authenticate } from "./authenticate.js";
+export { default as uploadMulter } from "./uploadMulter.js";
